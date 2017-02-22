8528a5f3505e20a039f55ba3ee3f65e9dbafeac5
Jens Törnell <jens.tornell@gmail.com>
2017-02-22 15:13:43 +0100

    Added text/plain

--- kirby-robots-writer.php
+++ kirby-robots-writer.php
@@ -4,7 +4,7 @@ kirby()->routes(array(
 		'pattern' => 'robots.txt',
 		'action' => function() {
 			if(c::get('robots.txt')) {
-				return new Response(trim(c::get('robots.txt')), 'html', 200);
+				return new Response(trim(c::get('robots.txt')), 'text/plain', 200);
 			}
 			return site()->visit(site()->errorPage());
 		}
